5b581f246a7256cf75634e7b53e474df4a79bbed
Marcus <pavlolins@gmail.com>
2025-10-18 17:48:05 -0300

    docs: marcar Módulo 1 como concluído — fundamentos da API .NET e IActionResult (18/10/2025)

--- Modulo-01-ApiFundamentos/ApiFundamentos/Controllers/HelloController.cs
+++ Modulo-01-ApiFundamentos/ApiFundamentos/Controllers/HelloController.cs
@@ -1,3 +1,4 @@
+using ApiFundamentos.DTOs;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,15 +19,39 @@ public class HelloController : ControllerBase
         return Ok(response);
     }
 
-    [HttpGet("{name}")]
+    [HttpGet("{name}")] // -> com parâmetros
     public IActionResult GetName(string name)
-    
+
     {
-    var response = new
-    {
-        message = $"Hello, {name}!"
-    };
-    return Ok(response);
-        
+        var response = new
+        {
+            message = $"Hello, {name}!"
+        };
+        return Ok(response);
+
     }
+
+    [HttpGet("querytest")] // → com QueryString
+    public IActionResult GetWithQueryTest([FromQuery] string? Name)
+    {
+        if (string.IsNullOrEmpty(Name))
+        {
+            return Ok(new { message = "Nenhum nome foi encontrado" });
+        }
+        return Ok(new { message = $"Olá {Name}, este é um teste de QueryString" });
+    }
+    
+[HttpPost("create")]
+public IActionResult CreateUser([FromBody] UserDto user)
+{
+    if (string.IsNullOrWhiteSpace(user.Name))
+        return BadRequest(new { error = "O campo 'Name' é obrigatório." });
+
+    return Created($"/api/hello/{user.Name}", new
+    {
+        message = $"Usuário {user.Name} com {user.Age} anos criado com sucesso!"
+    });
+}
+
+
 }
---
+++ Modulo-01-ApiFundamentos/ApiFundamentos/DTOs/UserDto.cs
@@ -0,0 +1,7 @@
+namespace ApiFundamentos.DTOs;
+
+public class UserDto
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
